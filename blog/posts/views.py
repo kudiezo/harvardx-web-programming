cf1c61bf035b61c59b02bd44bde2f52346b8c902
kudiezo <kudiezo527@gmail.com>
2024-09-18 11:49:37 +0100

    posts project finished

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def index(request, qtd_posts):
     res = []
 
-    for i in range(1, qtd_posts):
-        res.append("a"+"1")
+    for i in range(qtd_posts-9, qtd_posts+1):
+        res.append(i)
 
     return JsonResponse(res, safe=False)
